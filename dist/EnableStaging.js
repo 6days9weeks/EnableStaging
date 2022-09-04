7be4ecdd5be2a30c463395504c95bc2b4a9b88fc
6days9weeks <no-reply@6days9weeks.wtf>
2022-09-04 20:18:12 +0600

    Fix: Only for Discord/Enmity 142/143

--- dist/EnableStaging.js
+++ dist/EnableStaging.js
@@ -1,132 +1,46 @@
-function c(e) {
-  window.enmity.plugins.registerPlugin(e);
+function d(o) {
+  window.enmity.plugins.registerPlugin(o);
 }
-function w(e, s) {
-  return window.enmity.modules.getModule(e, s);
+function i(...o) {
+  return window.enmity.modules.getByProps(...o);
 }
-function i(...e) {
-  return window.enmity.modules.getByProps(...e);
+const s = window.enmity.modules.common.Dialog;
+const { native: e } = window.enmity;
+function w() {
+  e.reload();
 }
-window.enmity.modules.common,
-  window.enmity.modules.common.Constants,
-  window.enmity.modules.common.Clipboard,
-  window.enmity.modules.common.Assets,
-  window.enmity.modules.common.Messages,
-  window.enmity.modules.common.Clyde,
-  window.enmity.modules.common.Avatars,
-  window.enmity.modules.common.Native,
-  window.enmity.modules.common.React,
-  window.enmity.modules.common.Dispatcher,
-  window.enmity.modules.common.Storage,
-  window.enmity.modules.common.Toasts;
-const u = window.enmity.modules.common.Dialog;
-window.enmity.modules.common.Token,
-  window.enmity.modules.common.REST,
-  window.enmity.modules.common.Settings,
-  window.enmity.modules.common.Users,
-  window.enmity.modules.common.Navigation,
-  window.enmity.modules.common.NavigationNative,
-  window.enmity.modules.common.NavigationStack,
-  window.enmity.modules.common.Theme,
-  window.enmity.modules.common.Linking,
-  window.enmity.modules.common.StyleSheet,
-  window.enmity.modules.common.ColorMap,
-  window.enmity.modules.common.Components,
-  window.enmity.modules.common.Locale,
-  window.enmity.modules.common.Profiles,
-  window.enmity.modules.common.Lodash,
-  window.enmity.modules.common.Logger,
-  window.enmity.modules.common.Flux,
-  window.enmity.modules.common.SVG,
-  window.enmity.modules.common.Scenes,
-  window.enmity.modules.common.Moment;
-const { native: t } = window.enmity;
-function y() {
-  t.reload();
-}
-t.version, t.build, t.device, t.version;
-function r(e) {
-  return window.enmity.assets.getIDByName(e);
-}
-const l = w(
-  (e) =>
-    e.open !== void 0 &&
-    e.close !== void 0 &&
-    !e.openLazy &&
-    !e.startDrag &&
-    !e.init &&
-    !e.openReplay &&
-    !e.openChannelCallPopout
-),
-  g = {
-    name: "EnableStaging",
-    version: "2.0.1",
-    description: "Bypasses experiment gate. Fuck you aj.",
-    authors: [{ name: "dia ♡", id: "696828906191454221" }],
-    color: "#a0939d",
-    onStart() {
-      const e = () => {
-        const o = i("getUsers"),
-          m = Object.values(
-            i("isDeveloper")._dispatcher._actionHandlers._dependencyGraph
-              .nodes
-          );
-        try {
-          m.find(
-            (n) => n.name === "ExperimentStore"
-          ).actionHandler.OVERLAY_INITIALIZE({ user: { flags: 1 } });
-        } catch { }
-        const d = o.getCurrentUser;
-        (o.getCurrentUser = () => ({ hasFlag: () => !0 })),
-          m
-            .find((n) => n.name === "DeveloperExperimentStore")
-            .actionHandler.OVERLAY_INITIALIZE(),
-          (o.getCurrentUser = d);
-      },
-        s = () => {
-          l.open({
-            content: "Trying to enable experiments...",
-            source: r("debug"),
-          });
-          const o = i("getUsers"),
-            m = Object.values(
-              i("isDeveloper")._dispatcher._actionHandlers._dependencyGraph
-                .nodes
-            );
-          try {
-            m.find(
-              (n) => n.name === "ExperimentStore"
-            ).actionHandler.OVERLAY_INITIALIZE({ user: { flags: 1 } });
-          } catch { }
-          const d = o.getCurrentUser;
-          (o.getCurrentUser = () => ({ hasFlag: () => !0 })),
-            m
-              .find((n) => n.name === "DeveloperExperimentStore")
-              .actionHandler.OVERLAY_INITIALIZE(),
-            (o.getCurrentUser = d),
-            l.open({
-              content: "Experiments have been successfully enabled.",
-              source: r("Check"),
-            });
-        },
-        a = () => {
-          e(),
-            setTimeout(() => {
-              s();
-            }, 100);
-        };
-      setTimeout(() => {
-        a();
-      }, 300);
-    },
-    onStop() {
-      u.show({
-        title: "Experiments Disabled.",
-        body: "Disabling Experiments requires a restart, would you like to restart Discord?",
-        confirmText: "Yes",
-        cancelText: "No",
-        onConfirm: y,
-      });
-    },
-  };
-c(g);
+e.version, e.build, e.device, e.version;
+const l = {
+  name: "EnableStaging",
+  version: "2.0.2",
+  description: "Bypasses experiment gate. Fuck you aj.",
+  authors: [{ name: "dia \u2661", id: "696828906191454221" }],
+  color: "#a0939d",
+  onStart() {
+    const o = i("getUsers"),
+      m = Object.values(
+        i("isDeveloper")._dispatcher._actionHandlers._dependencyGraph.nodes
+      );
+    try {
+      m.find(
+        (n) => n.name === "ExperimentStore"
+      ).actionHandler.OVERLAY_INITIALIZE({ user: { flags: 1 } });
+    } catch { }
+    const t = o.getCurrentUser;
+    (o.getCurrentUser = () => ({ hasFlag: () => !0 })),
+      m
+        .find((n) => n.name === "DeveloperExperimentStore")
+        .actionHandler.OVERLAY_INITIALIZE(),
+      (o.getCurrentUser = t);
+  },
+  onStop() {
+    s.show({
+      title: "Experiments Disabled.",
+      body: "Disabling Experiments requires a restart, would you like to restart Discord?",
+      confirmText: "Yes",
+      cancelText: "No",
+      onConfirm: w,
+    });
+  },
+};
+d(l);
--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,12 @@
 import { Plugin, registerPlugin } from "enmity/managers/plugins";
-import { getByProps, getModule } from "enmity/metro";
+import { getByProps } from "enmity/metro";
 import { Dialog } from "enmity/metro/common";
 import { reload } from "enmity/api/native";
-import * as Assets from "enmity/api/assets";
-const Toast = getModule(
-  (m) =>
-    m.open !== undefined &&
-    m.close !== undefined &&
-    !m.openLazy &&
-    !m.startDrag &&
-    !m.init &&
-    !m.openReplay &&
-    !m.openChannelCallPopout
-);
 
 const EnableStaging: Plugin = {
   ...{
     name: "EnableStaging",
-    version: "2.0.1",
+    version: "2.0.2",
     description: "Bypasses experiment gate. Fuck you aj.",
     authors: [
       {
@@ -29,65 +18,24 @@ const EnableStaging: Plugin = {
   },
 
   onStart() {
-    // patch experiments twice because discord :german:
-    const PatchOne = () => {
-      const UserStore = getByProps("getUsers");
-      const nodes = Object.values(
-        getByProps("isDeveloper")._dispatcher._actionHandlers._dependencyGraph
-          .nodes
-      );
-      try {
-        nodes
-          .find((x) => x.name === "ExperimentStore")
-          .actionHandler["OVERLAY_INITIALIZE"]({ user: { flags: 1 } });
-      } catch {}
-      const oldUser = UserStore.getCurrentUser;
-      UserStore.getCurrentUser = () => {
-        return { hasFlag: () => true };
-      };
+    const UserStore = getByProps("getUsers");
+    const nodes = Object.values(
+      getByProps("isDeveloper")._dispatcher._actionHandlers._dependencyGraph
+        .nodes
+    );
+    try {
       nodes
-        .find((x) => x.name === "DeveloperExperimentStore")
-        .actionHandler["OVERLAY_INITIALIZE"]();
-      UserStore.getCurrentUser = oldUser;
+        .find((x) => x.name === "ExperimentStore")
+        .actionHandler["OVERLAY_INITIALIZE"]({ user: { flags: 1 } });
+    } catch {}
+    const oldUser = UserStore.getCurrentUser;
+    UserStore.getCurrentUser = () => {
+      return { hasFlag: () => true };
     };
-    const patchTwo = () => {
-      Toast.open({
-        content: `Trying to enable experiments...`,
-        source: Assets.getIDByName("debug"),
-      });
-      const UserStore = getByProps("getUsers");
-      const nodes = Object.values(
-        getByProps("isDeveloper")._dispatcher._actionHandlers._dependencyGraph
-          .nodes
-      );
-      try {
-        nodes
-          .find((x) => x.name === "ExperimentStore")
-          .actionHandler["OVERLAY_INITIALIZE"]({ user: { flags: 1 } });
-      } catch {}
-      const oldUser = UserStore.getCurrentUser;
-      UserStore.getCurrentUser = () => {
-        return { hasFlag: () => true };
-      };
-      nodes
-        .find((x) => x.name === "DeveloperExperimentStore")
-        .actionHandler["OVERLAY_INITIALIZE"]();
-      UserStore.getCurrentUser = oldUser;
-      Toast.open({
-        content: `Experiments have been successfully enabled.`,
-        source: Assets.getIDByName("Check"),
-      });
-    };
-    const plugin = () => {
-      PatchOne();
-      setTimeout(() => {
-        patchTwo();
-      }, 100); // give it some extra few ms to reload lol..
-    };
-
-    setTimeout(() => {
-      plugin();
-    }, 300); // give Flux some time to initialize -- 300ms should be more than enough
+    nodes
+      .find((x) => x.name === "DeveloperExperimentStore")
+      .actionHandler["OVERLAY_INITIALIZE"]();
+    UserStore.getCurrentUser = oldUser;
   },
 
   onStop() {
